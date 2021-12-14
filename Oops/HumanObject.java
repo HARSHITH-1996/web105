8f9f4ed7721c2c39c217d09e77604729d1542c76
HARSHITH-1996 <96135493+HARSHITH-1996@users.noreply.github.com>
2021-12-14 23:59:24 +0530

    Add files via upload

---
+++ Oops/HumanObject.java
@@ -0,0 +1,40 @@
+package Oops;
+
+public class HumanObject {
+
+	//Declaring Variables
+	String name;
+	String Gender;
+	double height;
+	//Creating Constructor
+	public HumanObject(String name, String gender, double height) {
+		super();
+		this.name = name;
+		Gender = gender;
+		this.height = height;
+	}
+	//Creating Method
+	void PersonDetails()
+	{
+		System.out.println("Person Name: "+name);
+		System.out.println("Person Gender: "+Gender);
+		System.out.println("Person height: "+height);
+	}
+	public static void main(String[] args) 
+	{
+		//Creating Objects for class
+		HumanObject obj1=new HumanObject("Harhith","M",6);
+		HumanObject obj2=new HumanObject("Ganesh","M",6);
+		HumanObject obj3=new HumanObject("Akshay","M",6);
+		//Invoking or calling methods
+		System.out.println("=========================");
+		obj1.PersonDetails();
+		System.out.println("=========================");
+		obj2.PersonDetails();
+		System.out.println("=========================");
+		obj3.PersonDetails();
+		System.out.println("=========================");
+	}
+	
+
+}
