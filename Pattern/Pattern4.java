e84dd8197db02612ea618cc5cb9dac1fe583d1a6
HARSHITH-1996 <96135493+HARSHITH-1996@users.noreply.github.com>
2021-12-14 23:58:20 +0530

    Add files via upload

---
+++ Pattern/Pattern4.java
@@ -0,0 +1,29 @@
+package Pattern;
+
+public class Pattern4 {
+
+	public static void patterns(int n)
+	{
+		for(int i=1;i<=5;i++)
+		{
+			for(int k=1;k<=5-i;k++)
+			{
+			System.out.print(" ");
+			}
+			for(int j=1;j<=i;j++)
+			{
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+	}
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n=5;
+		patterns(n);
+
+	}
+	
+}
